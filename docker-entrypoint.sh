540d0283cdfa42c70d2ce202f78983be7668d117
ilanyu <lanyu19950316@gmail.com>
2018-03-08 21:00:09 +0800

    init

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
 if [ -z "$JAVA_HOME" ]; then
-	export JAVA_HOME=/usr/local/jdk1.8.0_161
-	export PATH=$PATH:$JAVA_HOME/bin
+	echo export JAVA_HOME=/usr/local/jdk1.8.0_161 > /etc/profile.d/java.sh
+	echo export PATH=$PATH:$JAVA_HOME/bin >> /etc/profile.d/java.sh
+	source /etc/profile.d/java.sh
 fi
 
 if [ -z "$HADOOP_HOME" ]; then
-	export HADOOP_HOME=/usr/local/hadoop-2.7.4
-	export PATH=$PATH:$HADOOP_HOME/bin:$HADOOP_HOME/sbin
+	echo export HADOOP_HOME=/usr/local/hadoop-2.7.4 > /etc/profile.d/hadoop.sh
+	echo export PATH=$PATH:$HADOOP_HOME/bin:$HADOOP_HOME/sbin >> /etc/profile.d/hadoop.sh
+	source /etc/profile.d/hadoop.sh
 fi
 
 if [ -z "$HADOOP_COMMON_LIB_NATIVE_DIR" ]; then
-	export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native
+	echo export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native >> /etc/profile.d/hadoop.sh
+	source /etc/profile.d/hadoop.sh
 fi
 
 if [ -z "$HBASE_HOME" ]; then
-	export HBASE_HOME=/usr/local/hbase-1.2.6
-	export PATH=$PATH:$HBASE_HOME/bin
+	echo export HBASE_HOME=/usr/local/hbase-1.2.6 > /etc/profile.d/hbase.sh
+	echo export PATH=$PATH:$HBASE_HOME/bin >> /etc/profile.d/hbase.sh
+	source /etc/profile.d/hbase.sh
 fi
 
 if [ ! -f ~/.ssh/authorized_keys ]; then
